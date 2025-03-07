9405da8858ec1de9f068b1ea8dbc26d7bab7121f
GHADAHIOS <ghada515alenzi515@gmail.com>
2025-03-07 13:03:16 +0300

    Initial Commit

---
+++ FinalBus/FinalBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalBusApp.swift
+//  FinalBus
+//
+//  Created by GHADAH ALENEZI on 07/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct FinalBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinalBusTests/FinalBusTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinalBusTests.swift
+//  FinalBusTests
+//
+//  Created by GHADAH ALENEZI on 07/09/1446 AH.
+//
+
+import Testing
+@testable import FinalBus
+
+struct FinalBusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
